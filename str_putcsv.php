d59c7d3e1aea4b19c41dfb1c2a0b12659d3e153b
熊建江 <bear@xiongjianjiangdeMacBook-Air.local>
2016-01-04 17:30:54 +0800

    add str_putcsv

---
+++ str_putcsv.php
@@ -0,0 +1,25 @@
+<?php
+/* From: http://www.php.net/manual/en/function.str-getcsv.php#88773 and http://www.php.net/manual/en/function.str-getcsv.php#91170 */
+/*
+ * @see https://gist.github.com/2894568
+ */
+if(!function_exists('str_putcsv'))
+{
+    function str_putcsv($input, $delimiter = ',', $enclosure = '"')
+    {
+        // Open a memory "file" for read/write...
+        $fp = fopen('php://temp', 'r+');
+        // ... write the $input array to the "file" using fputcsv()...
+        fputcsv($fp, $input, $delimiter, $enclosure);
+        // ... rewind the "file" so we can read what we just wrote...
+        rewind($fp);
+        // ... read the entire line into a variable...
+        $data = fread($fp, 1048576);
+        // ... close the "file"...
+        fclose($fp);
+        // ... and return the $data to the caller, with the trailing newline from fgets() removed.
+        return rtrim($data, "\n");
+    }
+}
+
+echo str_putcsv(['"a','\"b','b好,cd']);
